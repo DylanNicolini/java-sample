b28728e04ce125213e08c218680fbe1bac8143e3
Dylan Nicolini <Dylan.Nicolini@Gmail.com>
2022-08-10 22:16:14 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Computer c = new Computer
+                .ComputerBulider("1TB","8GB")
+                .setBluetoothEnabled(true)
+                .setGraphicsCardEnabled(true)
+                .Builder();
+
+        System.out.println(c);
+
+
+    }
+}
